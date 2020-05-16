eaab2a2064fc20bedc23180cecca36717d87bc84
paulopplisboa <silva.pff@gmail.com>
2020-05-16 10:28:19 +0100

    Projecto criado

---
+++ src/test/java/com/Paulo/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Paulo.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
