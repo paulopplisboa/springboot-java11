a137ec244a805bf9d0271c51651e22fb0a7e38ab
paulopplisboa <silva.pff@gmail.com>
2020-05-19 09:24:05 +0100

    Payment, one-to-one association

--- src/main/java/com/Paulo/curso/config/TestConfig.java
+++ src/main/java/com/Paulo/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import com.Paulo.curso.entities.Category;
 import com.Paulo.curso.entities.Order;
 import com.Paulo.curso.entities.OrderItem;
+import com.Paulo.curso.entities.Payment;
 import com.Paulo.curso.entities.Product;
 import com.Paulo.curso.entities.User;
 import com.Paulo.curso.entities.enums.OrderStatus;
@@ -79,5 +80,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1); 
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+				
+				
 	}
 }
